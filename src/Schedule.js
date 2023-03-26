894ff71299d4239b8b3e4dd6f5f6dc5bbd6cc993
kongr45gpen <electrovesta@gmail.com>
2023-03-26 01:53:53 +0100

    Add animations for new items

--- src/Schedule.js
+++ src/Schedule.js
@@ -17,8 +17,27 @@ import ToggleButton from '@mui/material/ToggleButton';
 import ToggleButtonGroup from '@mui/material/ToggleButtonGroup';
 import TodayIcon from '@mui/icons-material/Today';
 import { useParams } from "react-router-dom";
-import { groupBy } from 'lodash';
+import { groupBy, shuffle, clone } from 'lodash';
+import { Flipper, Flipped } from 'react-flip-toolkit'
+import anime from "animejs";
 
+const animateElementIn = (el, i) =>
+    anime({
+        targets: el,
+        opacity: 1,
+        easing: "easeOutSine",
+        duration: 500
+    });
+
+const animateElementOut = (el, i, onComplete) => {
+    anime({
+        targets: el,
+        opacity: 0,
+        easing: "easeOutSine",
+        complete: onComplete,
+        duration: 500
+    });
+};
 
 const StyledToggleButtonGroup = styled(ToggleButtonGroup)(({ theme }) => ({
     '& .MuiToggleButtonGroup-grouped': {
@@ -57,50 +76,54 @@ function Selector({ days }) {
             onChange={(e, v) => { setAlignment(v); }}
             aria-label="Date"
         >
-            <ToggleButton disabled={true} aria-hidden="true">
+            <ToggleButton disabled={true} aria-hidden="true" value="">
                 <TodayIcon />
             </ToggleButton>
             {days.map((day, index) => {
                 const date = Moment(day);
                 const value = date.format('dddd').toLowerCase();
-                return <ToggleButton value={value} component={Link} to={`/schedule/` + value}>
-                    { date.format('dddd d MMM')}
+                return <ToggleButton key={value} value={value} component={Link} to={`/schedule/` + value}>
+                    {date.format('dddd d MMM')}
                 </ToggleButton>;
             })}
         </StyledToggleButtonGroup></Stack>;
 }
 
-function EventInSchedule({ id, event }) {
-    return <Paper key={id} sx={{ textDecoration: 'none', backgroundColor: stringToColor(event.room.data.id) }} elevation={5} component={Link} to={`event/` + id}>
-        <Grid container spacing={0} p={2}>
-            <Grid xl={4} xs={5} pr={3}>
-                <Typography className="schedule-time" sx={{ fontSize: "2.5rem", fontWeight: 400, textAlign: { xs: "center", md: "left" } }}>
-                    {Moment(event.start).format('HH:mm')}
-                    {" "}&ndash;{" "}
-                    {Moment(event.end).format('HH:mm')}
-                </Typography>
-            </Grid>
-            <Grid xl={8} xs={7}>
-                <Typography sx={{ fontWeight: 300, fontSize: { xs: "1.5rem", sm: "2rem", md: "2.5rem" } }}>
-                    {event.name}
-                </Typography>
-            </Grid>
-            <Grid xl={4} xs={5} className="schedule-room">
-                <Stack direction={{ xs: "column", md: "row" }} spacing={2} mr={3}>
-                    <TypeChip event={event} />
-                    <Chip label={event.room.data.attributes.name} />
-                </Stack>
-            </Grid>
-            <Grid xl={8} xs={7} className="schedule-speakers">
-                <Stack direction="row" spacing={2}>
-                    {event.speakers.data.map((s) => <div key={s['id']}>
-                        <Avatar src={s['attributes'].picture.data['attributes'].formats.thumbnail.url} className="schedule-avatar" />
-                        {s['attributes'].name}
-                    </div>)}
-                </Stack>
-            </Grid>
-        </Grid>
-    </Paper>;
+function EventInSchedule({ idd, event }) {
+    return <Flipped flipId={idd} onAppear={animateElementIn} onExit={animateElementOut}>
+        <Box sx={{ pt: 3 }}>
+            <Paper sx={{ textDecoration: 'none', backgroundColor: stringToColor(event.room.data.id), display: 'block' }} elevation={5} component={Link} to={`event/` + idd}>
+                <Grid container spacing={0} p={2}>
+                    <Grid xl={4} xs={5} pr={3}>
+                        <Typography className="schedule-time" sx={{ fontSize: "2.5rem", fontWeight: 400, textAlign: { xs: "center", md: "left" } }}>
+                            {Moment(event.start).format('HH:mm')}
+                            {" "}&ndash;{" "}
+                            {Moment(event.end).format('HH:mm')}
+                        </Typography>
+                    </Grid>
+                    <Grid xl={8} xs={7}>
+                        <Typography sx={{ fontWeight: 300, fontSize: { xs: "1.5rem", sm: "2rem", md: "2.5rem" } }}>
+                            {event.name}
+                        </Typography>
+                    </Grid>
+                    <Grid xl={4} xs={5} className="schedule-room">
+                        <Stack direction={{ xs: "column", md: "row" }} spacing={2} mr={3}>
+                            <TypeChip event={event} />
+                            <Chip label={event.room.data.attributes.name} />
+                        </Stack>
+                    </Grid>
+                    <Grid xl={8} xs={7} className="schedule-speakers">
+                        <Stack direction="row" spacing={2}>
+                            {event.speakers.data.map((s) => <div key={s['id']}>
+                                <Avatar src={s['attributes'].picture.data['attributes'].formats.thumbnail.url} className="schedule-avatar" />
+                                {s['attributes'].name}
+                            </div>)}
+                        </Stack>
+                    </Grid>
+                </Grid>
+            </Paper>
+        </Box>
+    </Flipped>;
 }
 
 function filterEvents(events, filterWord) {
@@ -108,31 +131,57 @@ function filterEvents(events, filterWord) {
 }
 
 export default function Schedule() {
+    const { filter } = useParams();
     const { events } = useData();
-    let { filter } = useParams();
-
     const [eventsByDay, setEventsByDay] = useState({});
 
-
     useEffect(() => {
-        console.log("grouping");
-
         if (!events) return;
 
         setEventsByDay(groupBy(events.data, (e) => Moment(e.attributes.start).format('YYYY-MM-DD')));
     }, [events]);
 
-    if (!filter) filter = "";
+    const [filteredEvents, setFilteredEvents] = useState([]);
+
+    useEffect(() => {
+        if (!events) return;
+
+        if (filter === undefined) {
+            setFilteredEvents(events['data']);
+        } else {
+            const filteredEvents = filterEvents(events['data'], filter);
+            setFilteredEvents(filteredEvents);
+        }
+    }, [events, filter]);
+
+    useEffect(() => {
+        console.log("shuffling/ue/called2", filteredEvents);
+    }, [filteredEvents]);
+
+    const simultaneousAnimations = ({
+        hideEnteringElements,
+        animateEnteringElements,
+        animateExitingElements,
+        animateFlippedElements
+    }) => {
+        hideEnteringElements();
+        animateExitingElements();
+        animateFlippedElements();
+        animateEnteringElements();
+    };
 
     return (
         <div>
             <h1>Schedule</h1>
             <Stack spacing={2} direction="row" justifyContent="center" mb={3}>
-                <Selector days={ Object.keys(eventsByDay) } />
+                <Selector days={eventsByDay && Object.keys(eventsByDay)} />
             </Stack>
-            <Stack spacing={2}>
-                {events && filterEvents(events['data'], filter).map((e) => <EventInSchedule key={e.id} id={e.id} event={e.attributes} />)}
-            </Stack>
+            <Flipper flipKey={filteredEvents.map((e) => e.id).join(' ')} handleEnterUpdateDelete={simultaneousAnimations}>
+                <Stack spacing={0}>
+                    {/* Spacing set to 0 and top margins used on childs, because Flipper throws these elements inside and messes up the margins. */}
+                    {filteredEvents && filteredEvents.map((e) => <EventInSchedule key={e.id} idd={e.id} event={e.attributes} />)}
+                </Stack>
+            </Flipper>
         </div>
     );
 }
