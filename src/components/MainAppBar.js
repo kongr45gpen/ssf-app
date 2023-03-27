8f09ab215bc0a4153d704bccd3504099bc17eb21
kongr45gpen <electrovesta@gmail.com>
2023-03-27 03:41:14 +0200

    Add breadcrumbs
    Closes #26

--- src/components/MainAppBar.js
+++ src/components/MainAppBar.js
@@ -8,9 +8,69 @@ import IconButton from '@mui/material/IconButton';
 import SearchIcon from '@mui/icons-material/Search';
 import ArrowBackIosNewIcon from '@mui/icons-material/ArrowBackIosNew';
 import { useData } from '../DataContext';
-import { useLoaderData, useLocation, useMatch, useMatches, useNavigate } from 'react-router-dom';
+import Link from '@mui/material/Link';
+import { Link as RouterLink, useLoaderData, useLocation, useMatch, useMatches, useNavigate } from 'react-router-dom';
 import { useState, useEffect } from 'react';
-import { alpha, InputBase, styled } from '@mui/material';
+import { alpha, Breadcrumbs, InputBase, styled, useMediaQuery, useTheme } from '@mui/material';
+import { ExpandLess } from '@mui/icons-material';
+
+const breadcrumbNameMap = {
+  '/schedule': 'Schedule',
+  '/schedule/event': 'Event',
+  '/map': 'Map',
+};
+
+function LinkRouter(props) {
+  return <Link {...props} component={RouterLink} />;
+}
+
+const StyledBreadcrumbs = styled(Breadcrumbs)(({ theme }) => ({
+  overflow: 'hidden',
+  textOverflow: 'ellipsis',
+  whiteSpace: 'nowrap',
+  '& > *': {
+    overflow: 'hidden',
+    textOverflow: 'ellipsis',
+    whiteSpace: 'nowrap',
+    flexWrap: 'nowrap!important',
+    justifyContent: 'flex-end',
+  },
+}));
+
+
+function Breadcrumb({ organisation }) {
+  const theme = useTheme();
+  const mediaQuery = useMediaQuery(theme.breakpoints.up('sm'));
+  const location = useLocation();
+  const pathnames = location.pathname.split('/').filter((x) => x);
+  const paths = pathnames
+    .map((v, index) => `/${pathnames.slice(0, index + 1).join('/')}`)
+    .filter((v) => Object.keys(breadcrumbNameMap).includes(v));
+
+  return (
+    <StyledBreadcrumbs aria-label="breadcrumb">
+      <LinkRouter underline="hover" color="inherit" to="/">
+        <Typography variant="h6" sx={{ overflow: 'hidden', textOverflow: 'ellipsis' }}>
+        {organisation && (mediaQuery ? organisation.event_name : organisation.event_short_name) }
+        </Typography>
+      </LinkRouter>
+      { 
+        paths.map((to, index) => {
+          let last = index === paths.length - 1;
+          return last ? (
+            <Typography color="text.primary" key={to}>
+              {breadcrumbNameMap[to]}
+            </Typography>
+          ) : (
+            <LinkRouter underline="hover" color="inherit" to={to} key={to}>
+              {breadcrumbNameMap[to]}
+            </LinkRouter>
+          );
+        })
+      }
+    </StyledBreadcrumbs>
+  );
+}
 
 const Search = styled('div')(({ theme }) => ({
   position: 'relative',
@@ -68,38 +128,36 @@ export default function MainAppBar() {
   const [backEnabled, setBackEnabled] = useState(false);
 
   useEffect(() => {
-     setBackEnabled(location && location[1] && location[1].handle && location[1].handle.back);
+    setBackEnabled(location && location[1] && location[1].handle && location[1].handle.back);
   }, [location]);
 
   return (
-    <Box sx={{ flexGrow: 1 }}>
-      <AppBar position="static">
-        <Toolbar>
-            <IconButton
-              size="large"
-              edge="start"
-              color="inherit"
-              aria-label="back"
-              sx={{ mr: 2, opacity: backEnabled ? 1 : 0 }}
-              onClick={() => navigate(-1)}
-              disabled={!backEnabled}
-            >
-              <ArrowBackIosNewIcon />
-            </IconButton>
-          <Typography variant="h6" component="div" sx={{ flexGrow: 1, whiteSpace: "nowrap", overflow: "hidden", textOverflow: 'ellipsis' }}>
-            {organisation && organisation.event_name}
-          </Typography>
-          <Search>
-            <SearchIconWrapper>
-              <SearchIcon />
-            </SearchIconWrapper>
-            <StyledInputBase
-              placeholder="Search…"
-              inputProps={{ 'aria-label': 'search' }}
-            />
-          </Search>
-        </Toolbar>
-      </AppBar>
-    </Box>
+    <AppBar position="sticky">
+      <Toolbar>
+        <IconButton
+          size="large"
+          edge="start"
+          color="inherit"
+          aria-label="back"
+          sx={{ mr: 2, opacity: backEnabled ? 1 : 0 }}
+          onClick={() => navigate(-1)}
+          disabled={!backEnabled}
+        >
+          <ArrowBackIosNewIcon />
+        </IconButton>
+        <Box sx={{ flexGrow: 1, overflow: 'hidden', whiteSpace: 'nowrap', display: 'flex' }}>
+          <Breadcrumb organisation={ organisation } />
+        </Box>
+        <Search>
+          <SearchIconWrapper>
+            <SearchIcon />
+          </SearchIconWrapper>
+          <StyledInputBase
+            placeholder="Search…"
+            inputProps={{ 'aria-label': 'search' }}
+          />
+        </Search>
+      </Toolbar>
+    </AppBar>
   );
 }
